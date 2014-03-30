79e125494d29b60fefeb2b53f94a852ec4e80b1c
app_4c459b7c <app_4c459b7c@ip-10-98-165-213.ec2.internal>
2014-03-30 20:18:34 +0000

    AndroidKickstartR generation

---
+++ Wetter/src/main/java/de/fhworms/inf1743/Thermometer.java
@@ -0,0 +1,25 @@
+
+package de.fhworms.inf1743;
+
+import android.app.Activity;
+import android.view.Menu;
+import com.googlecode.androidannotations.annotations.AfterViews;
+import com.googlecode.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.activity_main)
+public class Thermometer
+    extends Activity
+{
+
+
+    @AfterViews
+    void afterViews() {
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater();
+        return true;
+    }
+
+}
